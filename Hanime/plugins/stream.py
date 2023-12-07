82dcb80e11e2052ca02909939ccd14d03932efc0
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-12-07 20:49:45 +0530

    Update stream.py

--- Hanime/plugins/stream.py
+++ Hanime/plugins/stream.py
@@ -1,16 +1,20 @@
-from pytgcalls.pytgcalls import GroupCallFactory
+import os
+import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from pyrogram.raw.types import InputPeerChannel
+from pytgcalls import StreamType
 from Hanime import app, bot, music
 
-group_call_factory = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM)
 VIDEO_CALL = {}
 
 
 async def download_and_convert_video(reply_message, chat_id):
     try:
         video = await music.download_media(reply_message)
-        os.system(f'ffmpeg -i "{video}" -vn -f s16le -ac 2 -ar 48000 -acodec pcm_s16le -filter:a "atempo=0.81" vid-{chat_id}.raw -y')
+        os.system(
+            f'ffmpeg -i "{video}" -vn -f s16le -ac 2 -ar 48000 -acodec pcm_s16le -filter:a "atempo=0.81" vid-{chat_id}.raw -y'
+        )
         return True
     except Exception as e:
         print(f"Error during download and conversion: {e}")
@@ -29,10 +33,16 @@ async def stream_video(client, message):
         await message.reply("`Downloading...`")
         if await download_and_convert_video(replied_message, chat_id):
             await asyncio.sleep(5)  # Give some time before starting the stream
-            group_call = group_call_factory.get_file_group_call(f'vid-{chat_id}.raw')
-            await group_call.start(chat_id)
-            await group_call.set_video_capture(await music.download_media(replied_message))
-            VIDEO_CALL[chat_id] = group_call
+
+            # Use app.join_group_call with a play link and pulse_stream stream type
+            playlink = "https://hentaibar.onrender.com/video_83e23452.mp4"  # Replace with your actual playlink
+            await app.join_group_call(
+                chat_id,
+                playlink,
+                stream_type=StreamType().pulse_stream
+            )
+
+            VIDEO_CALL[chat_id] = True  # You can use this to track the active call
             await message.reply("**▶️ Started Streaming!**")
     except Exception as e:
         await message.reply(f"**Error during streaming: {e}**")
@@ -42,9 +52,14 @@ async def stream_video(client, message):
 async def stop_video(client, message):
     chat_id = message.chat.id
     try:
-        await VIDEO_CALL[chat_id].stop()
-        await message.reply("**⏹️ Stopped Streaming!**")
-        # Clean up temporary files after stopping the stream
-        os.remove(f'vid-{chat_id}.raw')
+        if VIDEO_CALL.get(chat_id):
+            await app.leave_group_call(chat_id)
+            VIDEO_CALL[chat_id] = False  # Update the status of the call
+            await message.reply("**⏹️ Stopped Streaming!**")
+
+            # Clean up temporary files after stopping the stream
+            os.remove(f'vid-{chat_id}.raw')
+        else:
+            await message.reply("**❌ No active stream to stop!**")
     except Exception as e:
         await message.reply(f"**Error during stopping the stream: {e}**")
