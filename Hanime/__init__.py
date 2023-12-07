59d29b9a38efad778e26a9215df483a86c185da3
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2023-12-07 16:42:15 +0530

    Update __init__.py

--- Hanime/__init__.py
+++ Hanime/__init__.py
@@ -7,8 +7,11 @@ from pytgcalls import PyTgCalls, idle
 
 FORMAT = "[INFO]: %(message)s"
 
-logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'),
-                                                    logging.StreamHandler()], format=FORMAT)
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[logging.FileHandler("logs.txt"), logging.StreamHandler()],
+    format=FORMAT,
+)
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -26,10 +29,22 @@ SESSION = os.getenv("SESSION")
 TOKEN = os.getenv("TOKEN")
 
 
-# PYROGRAM USER CLIENT 
-music = Client(name="music", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Hanime"))
+# PYROGRAM USER CLIENT
+music = Client(
+    name="music",
+    session_string=SESSION,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins=dict(root="Hanime"),
+)
 
-#PYROGRAM BOT CLIENT
-bot = Client(name="bot", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Hanime"))
-   
+# PYROGRAM BOT CLIENT
+bot = Client(
+    name="bot",
+    bot_token=TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins=dict(root="Hanime"),
+)
+
 app = PyTgCalls(music)
