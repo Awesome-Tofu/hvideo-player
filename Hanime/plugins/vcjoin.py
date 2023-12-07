ca78fed3090e85c82885eed68e76fed8d6100b73
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-12-07 18:45:02 +0530

    Update vcjoin.py

--- Hanime/plugins/vcjoin.py
+++ Hanime/plugins/vcjoin.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors import UserAlreadyParticipant
-from Hanime import app, bot
+from Hanime import music, bot
 
 
 @bot.on_message(filters.command(["join"]) & filters.group)
@@ -11,7 +11,7 @@ async def join_chat(c: Client, m: Message):
         invitelink = await c.export_chat_invite_link(chat_id)
         if invitelink.startswith("https://t.me/+"):
             invitelink = invitelink.replace("https://t.me/+", "https://t.me/joinchat/")
-            await app.join_chat(invitelink)
+            await music.join_chat(invitelink)
             return await bot.send_message(chat_id, "✅ Assistant joined chat")
     except UserAlreadyParticipant:
         return await bot.send_message(chat_id, "✅ Assistant already in chat")
