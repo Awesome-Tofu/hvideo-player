dc747f53c04f17b2d4de53ffd0fba3f1d4ebbf1d
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-12-07 19:58:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
-from Hanime import bot
+
 
 GROUP_ID = -1001849819947
 ASSISTANT_NAME = "BlackKnights_UB"
 UPDATES_CHANNEL = "botupdatex"
 SUPPORT_GROUP = "botsupportx"
-
-ok = bot.get_me()
-USERNAME = ok.username
-BOT_NAME = ok.first_name
+USERNAME = "CHOOSELEBOOBSBOT"
+BOT_NAME = "dudu hentai bot"
