bf952483060fed912ab0c31a7a78eedffb2d90ab
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-12-07 18:42:53 +0530

    Update __main__.py

--- Hanime/__main__.py
+++ Hanime/__main__.py
@@ -2,17 +2,21 @@ import pyrogram
 from pytgcalls import idle
 from Hanime import bot, app, music
 import config
-import asyncio
-
 
 async def run_clients():
     await bot.start()
     await app.start()
-    await music.start()
+
+    if not music.is_connected:
+        await music.start()
+
     await pyrogram.idle()
     await bot.send_message(chat_id=config.GROUP_ID, text="hello bot")
     await music.send_message(chat_id=config.GROUP_ID, text="hello app")
     await idle()
 
 if __name__ == "__main__":
+    import asyncio
     asyncio.get_event_loop().run_until_complete(run_clients())
+
+print("INFO: ALL CLIENTS STARTED SUCCESSFULLY")
