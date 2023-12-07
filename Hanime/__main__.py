2155646def15b3fa8e99033c73f989775f18da7a
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2023-12-07 16:43:07 +0530

    Update __main__.py

--- Hanime/__main__.py
+++ Hanime/__main__.py
@@ -3,20 +3,16 @@ from pytgcalls import idle
 from Hanime import bot, app, music
 import config
 
+
 async def run_clients():
     await bot.start()
     await app.start()
     await music.start()
-    await pyrogram.idle() 
-    await bot.send_message(
-        chat_id=config.GROUP_ID,
-        text="hello bot"
-    )
-    await music.send_message(
-        chat_id=config.GROUP_ID,
-        text="hello app"
-    )
+    await pyrogram.idle()
+    await bot.send_message(chat_id=config.GROUP_ID, text="hello bot")
+    await music.send_message(chat_id=config.GROUP_ID, text="hello app")
     await idle()
+
 
 if __name__ == "__main__":
     app.loop.run_until_complete(run_clients())
