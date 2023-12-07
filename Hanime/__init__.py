026c72741809d4f6e72cf9eed0c326f7eea1cb86
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-12-07 16:00:06 +0530

    Update __init__.py

--- Hanime/__init__.py
+++ Hanime/__init__.py
@@ -3,34 +3,12 @@ import time
 import logging
 from pyrogram import Client
 from pymongo import MongoClient
+from pytgcalls import PyTgCalls, idle
 
 FORMAT = "[INFO]: %(message)s"
 
 logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'),
                                                     logging.StreamHandler()], format=FORMAT)
-
-StartTime = time.time()
-
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-    while count < 4:
-        count += 1
-        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-    return ping_time
-
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -49,8 +27,9 @@ TOKEN = os.getenv("TOKEN")
 
 
 # PYROGRAM USER CLIENT 
-app = Client(name="app", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Hanime"))
+music = Client(name="music", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Hanime"))
 
 #PYROGRAM BOT CLIENT
 bot = Client(name="bot", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Hanime"))
    
+app = PyTgCalls(music)
