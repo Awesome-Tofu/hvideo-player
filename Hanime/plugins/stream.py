063db9c0085fcb911e3ac82d2c230e71089b0ee8
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-12-12 20:05:44 +0530

    Update stream.py

--- Hanime/plugins/stream.py
+++ Hanime/plugins/stream.py
@@ -3,68 +3,61 @@ import asyncio
 import subprocess
 from pyrogram import Client, filters
 from pytgcalls import StreamType
+from pytgcalls.types import AudioPiped
 from Hanime import app, bot, music
 
-VIDEO_CALL = {}
+api_url_hentai = "https://hentaibar.onrender.com/random"
 
-
-async def download_and_convert_video(reply_message, chat_id):
+async def fetch_hentai_data():
     try:
-        video = await music.download_media(reply_message)
-        subprocess.run([
-            'ffmpeg',
-            '-i', video,
-            '-vn', '-f', 's16le',
-            '-ac', '2', '-ar', '48000',
-            '-acodec', 'pcm_s16le',
-            '-filter:a', 'atempo=0.81',
-            f'vid-{chat_id}.raw', '-y'
-        ], check=True)
-        return True
-    except Exception as e:
-        print(f"Error during download and conversion: {e}")
-        return False
+        response = requests.get(api_url_hentai)
+        response.raise_for_status()  # Check for errors
 
+        hentai_data = response.json()
 
-@bot.on_message(filters.command("stream"))
-async def stream_video(client, message):
-    replied_message = message.reply_to_message
-    if not replied_message:
-        await message.reply("`Reply to some Video!`")
-        return
+        thumb_url = hentai_data.get('thum')
+        file_url = hentai_data.get('file')
+        name = hentai_data.get('name')
+        upload_date = hentai_data.get('upload_date')
+        duration = hentai_data.get('duration')
 
-    chat_id = message.chat.id
+        if thumb_url and file_url and name and upload_date and duration:
+            return {
+                'thumb_url': thumb_url,
+                'file_url': file_url,
+                'name': name,
+                'upload_date': upload_date,
+                'duration': duration
+            }
+        else:
+            return None
+
+    except requests.exceptions.RequestException as err:
+        return None
+
+@app.on_message(filters.command("play", prefixes="/"))
+async def play_command(client, message):
     try:
-        await message.reply("`Downloading...`")
-        if await download_and_convert_video(replied_message, chat_id):
-            await asyncio.sleep(5)  # Give some time before starting the stream
+        await message.reply("Please wait patiently. Fetching data for your request...")
 
-            # Use app.join_group_call with a play link and pulse_stream stream type
-            playlink = "https://hentaibar.onrender.com/video_83e23452.mp4"  # Replace with your actual playlink
-            await app.join_group_call(
+        hentai_data = await fetch_hentai_data()
+
+        if hentai_data:
+            thumb_url = hentai_data['thumb_url']
+            file_url = hentai_data['file_url']
+            name = hentai_data['name']
+            upload_date = hentai_data['upload_date']
+            duration = hentai_data['duration']
+
+            await message.reply_photo(thumb_url, caption=f"Name: {name}\nUpload Date: {upload_date}\nDuration: {duration}\n\nStreamlink: {file_url}")
+
+            app.join_group_call(
                 chat_id,
-                playlink,
-                stream_type=StreamType().pulse_stream
+                AudioPiped(file_url, stream_type=StreamType().pulse_stream)
             )
 
-            VIDEO_CALL[chat_id] = True  # You can use this to track the active call
-            await message.reply("**▶️ Started Streaming!**")
-    except Exception as e:
-        await message.reply(f"**Error during streaming: {e}**")
-
-
-@bot.on_message(filters.command("stop"))
-async def stop_video(client, message):
-    chat_id = message.chat.id
-    try:
-        if VIDEO_CALL.get(chat_id):
-            await app.leave_group_call(chat_id)
-            VIDEO_CALL[chat_id] = False  # Update the status of the call
-            await message.reply("**⏹️ Stopped Streaming!**")
-
-            # Clean up temporary files after stopping the stream
-            os.remove(f'vid-{chat_id}.raw')
         else:
-            await message.reply("**❌ No active stream to stop!**")
-    except Exception as e:
-        await message.reply(f"**Error during stopping the stream: {e}**")
+            await message.reply_text("Error: Incomplete data received from the API.")
+
+    except requests.exceptions.RequestException as err:
+        await message.reply_text(f"Error fetching data: {err}")
