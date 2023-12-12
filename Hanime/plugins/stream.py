86fe632d6903fdffa83bb1d5269ad3a52f178e57
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-12-12 20:44:07 +0530

    Update stream.py

--- Hanime/plugins/stream.py
+++ Hanime/plugins/stream.py
@@ -39,7 +39,7 @@ async def fetch_hentai_data():
 @bot.on_message(filters.command("play", prefixes="/"))
 async def play_command(client, message):
     try:
-        chat_id = message.chat.id
+        chat_id = -1001849819947
         await message.reply("Please wait patiently. Fetching data for your request...")
 
         hentai_data = await fetch_hentai_data()
