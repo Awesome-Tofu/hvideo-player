d83b98e81aaab9874dc8e642e774749328457c24
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-12-13 06:32:08 +0530

    Update stream.py

--- Hanime/plugins/stream.py
+++ Hanime/plugins/stream.py
@@ -7,60 +7,34 @@ from pytgcalls.types import AudioPiped, AudioVideoPiped
 from Hanime import app, bot, music
 import requests
 
-api_url_hentai = "https://hentaibar.onrender.com/random"
-
-async def fetch_hentai_data():
+@bot.on_message(filters.command(["play", "vplay"]) & filters.group)
+async def play_command(_, message):
     try:
-        response = requests.get(api_url_hentai)
-        response.raise_for_status()  # Check for errors
-
-        hentai_data = response.json()
-
-        thumb_url = hentai_data.get('thumbnailUrl')
-        file_url = hentai_data.get('file')
-        name = hentai_data.get('name')
-        upload_date = hentai_data.get('upload_date')
-        duration = hentai_data.get('duration')
-
-        if thumb_url and file_url and name and upload_date and duration:
-            return {
-                'thumb_url': thumb_url,
-                'file_url': file_url,
-                'name': name,
-                'upload_date': upload_date,
-                'duration': duration
-            }
-        else:
-            return None
-
-    except requests.exceptions.RequestException as err:
-        return None
-
-@bot.on_message(filters.command("play", prefixes="/"))
-async def play_command(client, message):
-    try:
+        link = message.text.split(None, 1)[1]
         chat_id = -1001849819947
-        await message.reply("Please wait patiently. Fetching data for your request...")
+        await message.delete()
+        state = message.command[0].lower()
 
-        hentai_data = await fetch_hentai_data()
+        if state == "saudio":
+            damn = AudioPiped
+            emj = "🎵"
+        elif state == "svideo":
+            damn = AudioVideoPiped
+            emj = "🎬"
+        m = await message.reply_text("🔄 ᴘʀᴏᴄᴇssɪɴɢ...")
 
-        if hentai_data:
-            thumb_url = hentai_data['thumb_url']
-            file_url = hentai_data['file_url']
-            name = hentai_data['name']
-            upload_date = hentai_data['upload_date']
-            duration = hentai_data['duration']
+        await app.join_group_call(
+            chat_id,
+            damn(link),
+            stream_type=StreamType().pulse_stream
+        )
+        await m.edit(f"{emj} sᴛᴀʀᴛᴇᴅ sᴛʀᴇᴀᴍɪɴɢ: [Link]({link})", disable_web_page_preview=True)
 
-            await message.reply_photo(thumb_url, caption=f"Name: {name}\nUpload Date: {upload_date}\nDuration: {duration}\n\nStreamlink: {file_url}")
-
-            await app.join_group_call(
-                chat_id,
-                # VideoPiped(file_url)
-                AudioVideoPiped(file_url)
-            )
-
-        else:
-            await message.reply_text("Error: Incomplete data received from the API.")
+    except Exception as e:
+        return await m.edit(str(e))
 
     except requests.exceptions.RequestException as err:
         await message.reply_text(f"Error fetching data: {err}")
+
+    else:
+        await message.reply_text("Error: kela")
