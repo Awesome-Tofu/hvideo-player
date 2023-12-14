610c756f51180102d84f661f3dfc27cc631934c4
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2023-12-14 19:00:38 +0530

    Update stream.py

--- Hanime/plugins/stream.py
+++ Hanime/plugins/stream.py
@@ -1,50 +1,35 @@
 import os
-import requests
+import asyncio
+import subprocess
 from pyrogram import Client, filters
 from pytgcalls import StreamType
-from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped, VideoPiped
+from pytgcalls.types import AudioPiped, VideoPiped
 from Hanime import app, bot, music
+import requests
 
-@bot.on_message(filters.command(["play", "vplay", "test"]) & filters.group)
+@bot.on_message(filters.command(["play", "vplay"]) & filters.group)
 async def play_command(_, message):
     try:
+        link = message.text.split(None, 1)[1]
         chat_id = -1001849819947
-        state = message.command[0].lower()
-        testfile = "test.mkv"
-
-        # Check if the message is a reply
-        if message.reply_to_message and (message.reply_to_message.audio or message.reply_to_message.video):
-            media = message.reply_to_message.audio or message.reply_to_message.video
-            file_id = media.file_id
-            file_path = await app.download_media(file_id)
-        else:
-            link = message.text.split(None, 1)[1]
-            file_path = f"downloaded_file.{link.split('.')[-1]}"
-            # Download the file to the server
-            response = requests.get(link, stream=True)
-            with open(file_path, 'wb') as file:
-                for chunk in response.iter_content(chunk_size=8192):
-                    file.write(chunk)
-
         await message.delete()
+        state = message.command[0].lower()
 
-        if state == "test":
-            damn = AudioVideoPiped
-            emj = "🎵🎬"
-            stream_file = testfile
-        else:
-            return await message.reply_text("Error: Invalid state")
-
+        if state == "play":
+            damn = AudioPiped
+            emj = "🎵"
+        elif state == "vplay":
+            damn = VideoPiped
+            emj = "🎬"
         m = await message.reply_text("🔄 ᴘʀᴏᴄᴇssɪɴɢ...")
-        
+
         await app.join_group_call(
             chat_id,
-            damn(file_path)
+            damn(link)
         )
-        await m.edit(f"{emj} sᴛᴀʀᴛᴇᴅ sᴛʀᴇᴀᴍɪɴɢ: [Link]({file_path})", disable_web_page_preview=True)
+        await m.edit(f"{emj} sᴛᴀʀᴛᴇᴅ sᴛʀᴇᴀᴍɪɴɢ: [Link]({link})", disable_web_page_preview=True)
 
     except Exception as e:
-        print(e)
         return await m.edit(str(e))
 
     except requests.exceptions.RequestException as err:
