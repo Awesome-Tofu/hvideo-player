55ef522ea9a261fdbcf4241a220b6bccbb8218d2
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2023-12-28 22:11:16 +0530

    Update stream.py

--- Hanime/plugins/stream.py
+++ Hanime/plugins/stream.py
@@ -1,5 +1,7 @@
 import os
 import asyncio
+import random
+import aiohttp
 import subprocess
 from pyrogram import Client, filters
 from pytgcalls import StreamType, idle
@@ -15,18 +17,32 @@ async def download_audio(url):
     return obj.get_dest()
 
 
-async def fetch_random_file():
-    random_url = "https://hentaibar.onrender.com/random"
-    response = requests.get(random_url)    
+async def fetch_hbar(url):
+    response = requests.get(url)    
     if response.status_code == 200:
         data = response.json()
         file_url = data.get("file")
         title = data.get("name")
         thumb_url = data.get("thumbnailUrl")
         duration = data.get("duration")
-        if file_url:
+        if data:
             return {"file_url": file_url, "title": title, "thumb_url": thumb_url, "duration": duration}
     return None
+
+async def fetch_hbar_search(query):
+    response = requests.get(f"https://hentaibar.onrender.com/search/{query}/1")    
+    if response.status_code == 200:
+        data = response.json()
+        result = random.choice(data["results"])
+        fetched_vid = await fetch_hbar(result["url"])
+        file_url = fetched_vid.get("file_url")
+        title = fetched_vid.get("title")
+        thumb_url = fetched_vid.get("thumb_url")
+        duration = fetched_vid.get("duration")
+        if data:
+            return {"file_url": file_url, "title": title, "thumb_url": thumb_url, "duration": duration}
+    return None    
+
 
 
 @bot.on_message(filters.command(["hplay"]) & filters.group)
@@ -38,13 +54,18 @@ async def hplay_command(_, message):
         state = message.command[0].lower()
 
         if len(message.command) > 1 and message.command[1].lower() == "random":
-            random_file = await fetch_random_file()
+            random_file = await fetch_hbar("https://hentaibar.onrender.com/random")
             link = random_file["file_url"]
             title = random_file["title"]
             thumb_url = random_file["thumb_url"]
             duration = random_file["duration"]
         else:
-            link = message.text.split(None, 1)[1]
+            query = message.text.split(None, 1)[1]
+            search_file = await fetch_hbar_search(query)
+            link = search_file["file_url"]
+            title = search_file["title"]
+            thumb_url = search_file["thumb_url"]
+            duration = search_file["duration"]
 
         audio_path = await download_audio(link)
         await app.join_group_call(
