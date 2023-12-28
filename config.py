2530a211e6697380819b365ccb15e4ca531e420c
DarkNemesis <126957307+Vishal-1756@users.noreply.github.com>
2023-12-28 22:49:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-API_ID = 9949701
-API_HASH = "47013db2802464f3d2c09593fb0dc880"
-TOKEN = "6085244632:AAGzRhYoXbw57AbsZx7M1Hbnda1M7Dk0WKU"
-SESSION = "BQCX0gUADTLzdx-SSwEItNlpoI-RhRP7YnztWHO2W50ExQNzmvI4daKgtnbK4vGqlcMi-XXN_7bBSM13hQz_fWfNjvsDVs7K0k_vdXeqCijjtQOz2tJgQ6bWlvp_8X0VTFYelNZbmxDx4UW6TMWLUEys-9r52YYoYYMvjILPjrzhmPHq6RxE6v0e215Ij1VUJ6fDjhbXUdX0CGIrnUj4fB5VLzvCh5ggPmSPQHAbnM0YJR8SCDgmnyp6oTrr9jVYe0Tb3vsRLYO1tal-XRyOz7p7iDSU4PLnCIopLAvB_b_-MVttL9cysQqJgtJC10n7FsBtpzaWiYKikTtW5HTW9CWSDRugTgAAAAEwKIdxAA"
+import os
+
+
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+TOKEN = os.getenv("TOKEN")
+SESSION = os.getenv("SESSION")
 DB_URL = "mongodb+srv://tofu:tofu@cluster2.gz7lfjd.mongodb.net/?retryWrites=true&w=majority"
 GROUP_ID = -1002107596400
 ASSISTANT_NAME = "monkeyDjoyboi"
